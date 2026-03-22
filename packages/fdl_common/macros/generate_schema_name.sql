121b452b7d87456282bcb0037b0006a0b6444dc0
UD <u.yudai1202@gmail.com>
2026-03-22 17:22:28 +0900

    fdl_common から generate_schema_name を削除
    dbt はパッケージ内の generate_schema_name オーバーライドを認識しない。
    ルートプロジェクトの macros/ に配置する必要があるため、
    パッケージから削除して誤解を防ぐ。

--- packages/fdl_common/macros/generate_schema_name.sql
+++
@@ -1,9 +0,0 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-  {%- if custom_schema_name is not none -%}
-    {{ custom_schema_name | trim }}
-  {%- elif node.fqn | length > 2 -%}
-    {{ node.fqn[1] }}
-  {%- else -%}
-    {{ target.schema }}
-  {%- endif -%}
-{%- endmacro %}
